2183836ba11e16aef01a5b46577a8b7fa5d49832
Mayuridudhat <mayuridudhat2006@gmail.com>
2026-04-01 01:30:33 +0530

    Rename Program2 class to program2

--- program2.java
+++ program2.java
@@ -44,7 +44,7 @@ class Consumer extends Thread {
     }
 }
 
-public class Program2 {
+public class program2 {
     public static void main(String[] args) {
         System.out.println("=== Producer-Consumer Problem ===");
         SharedBuffer buffer = new SharedBuffer();
