9d19b768df667530c9e21dc4a81497b78ceca9ed
Mayuridudhat <mayuridudhat2006@gmail.com>
2026-04-01 02:11:39 +0530

    Rename class Program7Client to program7Client

--- Program7Client.java
+++ Program7Client.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Program7Client {
+public class program7Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 5000);
